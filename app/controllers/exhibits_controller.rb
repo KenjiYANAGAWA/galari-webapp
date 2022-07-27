e06198daedd71075c21b0bfba144bcacbca0d38a
Kenji Yanagawa <kenchibi@gmail.com>
2022-07-27 17:52:55 +0900

    seed using csv

---
+++ app/controllers/exhibits_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :home ]
+  
+    def new
+      @exhibit = Exhibit.new
+    end
+
+    def create
+        @exhibit = Exhibit.new(exhibit_params)
+        @exhibit.save
+        redirect_to exhibit_path(@exhibit)
+    end
+
+    private
+
+def exhibit_params
+  params.require(:exhibit).permit(:title, :subtitle, :description, :explainer, :interactive, photos: [])
+end
+  end
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "csv"
+
+filepath = "exhibits.csv"
+puts "reading CSV file"
+CSV.foreach(filepath, headers: :first_row) do |row|
+    title = row['title']
+    subtitle = row['subtitle']
+    description = row['description']
+    explainer = row['explainer']
+    interactive = row['interactive'] == 'true'
+
+    img_file = File.open("app/assets/images/exhibit_thumbnails/#{title}.jpeg")
+    exhibit = Exhibit.create!(
+      title: title.gsub("_"," "),
+      subtitle: subtitle,
+      description: description,
+      explainer: explaiiner,
+      interactive: interactive
+    )
+    place.photos.attach(io: img_file, filename: "exhibit_thumbnail_#{title}.jpeg")
+    puts "#{exhibit.title} created"
+end
