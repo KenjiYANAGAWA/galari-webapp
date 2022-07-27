6ca06653a6110df25aec4c74d97c0a942fc70613
Kenji Yanagawa <kenchibi@gmail.com>
2022-07-27 16:42:20 +0900

    fixed syntax for string array

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @exhibits = %s(a i u e o)
+    @exhibits = %w(a i u e o)
   end
 end
